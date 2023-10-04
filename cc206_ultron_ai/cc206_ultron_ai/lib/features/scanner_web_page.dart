670413cae7c2e7336cc2a8ca60370209fb636f11
defnotyujine <124588725+defnotyujine@users.noreply.github.com>
2023-10-04 21:06:23 +0800

    initial commit garces, occeno

---
+++ cc206_ultron_ai/cc206_ultron_ai/lib/features/scanner_web_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class ScannerWebPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Image Scanner Web Page'),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              height: 300,
+              color: Colors.black,
+            ),
+
+            // Capture Button
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Capture'),
+            ),
+
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.all(16),
+                child: ListView.builder(
+                  itemCount: 5,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Card(
+                      margin: EdgeInsets.all(8),
+                      child: ListTile(
+                        leading: Icon(Icons.image),
+                        title: Text('Image $index'),
+                        onTap: () {},
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.settings),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: Icon(Icons.delete),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: Icon(Icons.share),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
