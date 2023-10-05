ec16a17d1e4e8d44a99549875b5c89404c3b8c11
defnotyujine <124588725+defnotyujine@users.noreply.github.com>
2023-10-05 21:25:03 +0800

    search bar function

--- cc206_ultron_ai/cc206_ultron_ai/lib/features/character_profiles.dart
+++ cc206_ultron_ai/cc206_ultron_ai/lib/features/character_profiles.dart
@@ -1,24 +1,90 @@
 import 'package:flutter/material.dart';
 
-class CharacterProfiles extends StatelessWidget {
+class CharacterProfiles extends StatefulWidget {
+  @override
+  _CharacterProfilesState createState() => _CharacterProfilesState();
+}
+
+class _CharacterProfilesState extends State<CharacterProfiles> {
+  TextEditingController _searchController = TextEditingController();
+
+  List<MarvelCharacter> _filteredCharacters = List.from(marvelCharacters);
+
+  @override
+  void initState() {
+    super.initState();
+    _searchController.addListener(_filterCharacterList);
+  }
+
+  void _filterCharacterList() {
+    String searchText = _searchController.text.toLowerCase();
+
+    setState(() {
+      _filteredCharacters = marvelCharacters.where((character) {
+        return character.name.toLowerCase().contains(searchText);
+      }).toList();
+    });
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Marvel Character Profiles'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.clear),
+            onPressed: () {
+              setState(() {
+                _searchController.clear();
+                _filteredCharacters = List.from(marvelCharacters);
+              });
+            },
+          ),
+        ],
       ),
-      body: ListView.builder(
-        itemCount: marvelCharacters.length,
-        itemBuilder: (context, index) {
-          final character = marvelCharacters[index];
-          return ListTile(
-            leading: CircleAvatar(
-              backgroundImage: AssetImage(character.imageAsset),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _searchController,
+              decoration: InputDecoration(
+                labelText: 'Search Characters',
+                prefixIcon: Icon(Icons.search),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.clear),
+                  onPressed: () {
+                    setState(() {
+                      _searchController.clear();
+                    });
+                  },
+                ),
+              ),
             ),
-            title: Text(character.name),
-            subtitle: Text(character.description),
-          );
-        },
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _filteredCharacters.length,
+              itemBuilder: (context, index) {
+                final character = _filteredCharacters[index];
+                return ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage(character.imageAsset),
+                  ),
+                  title: Text(character.name),
+                  subtitle: Text(character.description),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -63,3 +129,9 @@ final List<MarvelCharacter> marvelCharacters = [
     imageAsset: 'assets/doctorstrange.jpg',
   ),
 ];
+
+void main() {
+  runApp(MaterialApp(
+    home: CharacterProfiles(),
+  ));
+}
