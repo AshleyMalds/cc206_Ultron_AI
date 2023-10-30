abce3ce2f898a30c56c485efe93a8c9599808095
defnotyujine <124588725+defnotyujine@users.noreply.github.com>
2023-10-30 19:37:39 +0800

    Commit for the share function

--- cc206_ultron_ai/cc206_ultron_ai/lib/features/scanner_web_page.dart
+++ cc206_ultron_ai/cc206_ultron_ai/lib/features/scanner_web_page.dart
@@ -10,10 +10,8 @@ class ScannerWebPage extends StatelessWidget {
       ),
       body: Center(
         child: Container(
-          // Wrap the content with a Container
-          margin: EdgeInsets.symmetric(
-              horizontal: 200), // Add more horizontal margin
-          padding: EdgeInsets.all(16), // Add padding to the content
+          margin: EdgeInsets.symmetric(horizontal: 200),
+          padding: EdgeInsets.all(16),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
@@ -55,10 +53,66 @@ class ScannerWebPage extends StatelessWidget {
                     icon: Icon(Icons.delete),
                     onPressed: () {},
                   ),
-                  IconButton(
+                  PopupMenuButton<String>(
                     icon: Icon(Icons.share),
-                    onPressed: () {},
-                  ),
+                    onSelected: (String value) {
+                      if (value == 'Bluetooth') {
+                        // Handle Bluetooth sharing
+                      } else if (value == 'Gmail') {
+                        // Handle Gmail sharing
+                      } else if (value == 'ShareIt') {
+                        // Handle ShareIt sharing
+                      } else if (value == 'Yahoo') {
+                        // Handle Yahoo sharing
+                      }
+                      // Add more sharing options as needed
+                    },
+                    itemBuilder: (BuildContext context) {
+                      return <PopupMenuEntry<String>>[
+                        PopupMenuItem<String>(
+                          value: 'Bluetooth',
+                          child: Row(
+                            children: [
+                              Icon(Icons.bluetooth),
+                              SizedBox(width: 8), // Add some spacing
+                              Text('Bluetooth'),
+                            ],
+                          ),
+                        ),
+                        PopupMenuItem<String>(
+                          value: 'Gmail',
+                          child: Row(
+                            children: [
+                              Icon(Icons.email),
+                              SizedBox(width: 8), // Add some spacing
+                              Text('Gmail'),
+                            ],
+                          ),
+                        ),
+                        PopupMenuItem<String>(
+                          value: 'ShareIt',
+                          child: Row(
+                            children: [
+                              Icon(Icons.send),
+                              SizedBox(width: 8), // Add some spacing
+                              Text('ShareIt'),
+                            ],
+                          ),
+                        ),
+                        PopupMenuItem<String>(
+                          value: 'Yahoo',
+                          child: Row(
+                            children: [
+                              Icon(Icons.mail),
+                              SizedBox(width: 8), // Add some spacing
+                              Text('Yahoo'),
+                            ],
+                          ),
+                        ),
+                        // Add more sharing options as needed
+                      ];
+                    },
+                  )
                 ],
               ),
               ElevatedButton(
