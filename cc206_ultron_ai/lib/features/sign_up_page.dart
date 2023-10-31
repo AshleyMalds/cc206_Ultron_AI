a0961dbd738686acd528565590576cf7e1f2a4cc
AshleyMalds <124685540+AshleyMalds@users.noreply.github.com>
2023-10-31 23:19:01 +0800

    hero third

--- cc206_ultron_ai/lib/features/sign_up_page.dart
+++ cc206_ultron_ai/lib/features/sign_up_page.dart
@@ -96,21 +96,38 @@ class HeroProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const String heroImageAsset = "assets/captain_america.jpg";
-    const String heroBackstory = """
-  Steve Rogers was a frail young man who was rejected from the military because of his health. However, he was given the opportunity to participate in a secret experiment that would give him superhuman strength and agility. The experiment was a success, and Rogers became Captain America, a symbol of hope and freedom.
+    //const String heroImageAsset = "assets/captain_america.jpg";
+    //const String heroBackstory = """
+    // Steve Rogers was a frail young man who was rejected from the military because of his health. However, he was given the opportunity to participate in a secret experiment that would give him superhuman strength and agility. The experiment was a success, and Rogers became Captain America, a symbol of hope and freedom.
 
-  Captain America fought alongside the Allies in World War II, and he played a key role in defeating the Axis powers. After the war, he was frozen in ice for nearly seventy years. He was eventually revived, and he joined the Avengers, a team of superheroes who protect the world from threats.
+    // Captain America fought alongside the Allies in World War II, and he played a key role in defeating the Axis powers. After the war, he was frozen in ice for nearly seventy years. He was eventually revived, and he joined the Avengers, a team of superheroes who protect the world from threats.
 
-  Captain America is a true hero. He is brave, selfless, and always willing to fight for what is right. He is an inspiration to people all over the world.
-  """;
+    // Captain America is a true hero. He is brave, selfless, and always willing to fight for what is right. He is an inspiration to people all over the world.
+    // """;
 
     return Scaffold(
       appBar: AppBar(
         title: Text('${heroName} Profile'),
       ),
       body: Center(
-        child: Text('This is the profile page for ${heroName}.'),
+        child: Column(
+          children: [
+            Image.network('asset/captain_america.png'),
+            Text("""
+            Name: Steve Rogers
+
+            Age: 105
+
+            Superpower: None, Enhanced body strength, speed, agility, and endurance.
+
+Steve Rogers was a frail young man who was rejected from the military because of his health. However, he was given the opportunity to participate in a secret experiment that would give him superhuman strength and agility. The experiment was a success, and Rogers became Captain America, a symbol of hope and freedom. 
+        
+Captain America fought alongside the Allies in World War II, and he played a key role in defeating the Axis powers. After the war, he was frozen in ice for nearly seventy years. He was eventually revived, and he joined the Avengers, a team of superheroes who protect the world from threats.
+
+Captain America is a true hero. He is brave, selfless, and always willing to fight for what is right. He is an inspiration to people all over the world.
+  """),
+          ],
+        ),
       ),
     );
   }
