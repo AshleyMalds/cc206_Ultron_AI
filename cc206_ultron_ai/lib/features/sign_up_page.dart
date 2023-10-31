2f41223d3fec586900af05b7d95cd36b823ddd85
AshleyMalds <124685540+AshleyMalds@users.noreply.github.com>
2023-10-31 22:50:14 +0800

    hero second

--- cc206_ultron_ai/lib/features/sign_up_page.dart
+++ cc206_ultron_ai/lib/features/sign_up_page.dart
@@ -96,6 +96,15 @@ class HeroProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const String heroImageAsset = "assets/captain_america.jpg";
+    const String heroBackstory = """
+  Steve Rogers was a frail young man who was rejected from the military because of his health. However, he was given the opportunity to participate in a secret experiment that would give him superhuman strength and agility. The experiment was a success, and Rogers became Captain America, a symbol of hope and freedom.
+
+  Captain America fought alongside the Allies in World War II, and he played a key role in defeating the Axis powers. After the war, he was frozen in ice for nearly seventy years. He was eventually revived, and he joined the Avengers, a team of superheroes who protect the world from threats.
+
+  Captain America is a true hero. He is brave, selfless, and always willing to fight for what is right. He is an inspiration to people all over the world.
+  """;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('${heroName} Profile'),
