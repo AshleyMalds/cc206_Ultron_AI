cdeaf6ce14069843ebb6360bef7e0ca75022dc3e
AshleyMalds <124685540+AshleyMalds@users.noreply.github.com>
2023-12-02 23:11:41 +0800

    login update

--- cc206_ultron_ai/cc206_ultron_ai/lib/features/login_page.dart
+++ cc206_ultron_ai/cc206_ultron_ai/lib/features/login_page.dart
@@ -7,121 +7,162 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Log In'),
-        backgroundColor: Colors.red, // Set Marvel-themed app bar color
-      ),
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage(
-                'asset/superheroes.jpg'), // Marvel-themed background image
+            image: AssetImage('asset/superheroes.jpg'),
             fit: BoxFit.cover,
           ),
         ),
-        child: Container(
-          color:
-              Colors.black.withOpacity(0.7), // Set opacity for better viewing
-          padding: const EdgeInsets.all(16.0),
-          child: Center(
+        child: Center(
+          child: Container(
+            width: 480,
+            margin: EdgeInsets.only(top: 5),
+            padding: const EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              color: Colors.black.withOpacity(0.7),
+              borderRadius: BorderRadius.circular(10.0),
+            ),
             child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  // Login form fields
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: TextFormField(
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16.0,
-                      ),
-                      decoration: InputDecoration(
-                        labelText: 'Email',
-                        labelStyle: TextStyle(color: Colors.white),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.red),
-                        ),
-                        icon: Icon(Icons.email, color: Colors.white),
-                        contentPadding: EdgeInsets.symmetric(horizontal: 10.0),
-                      ),
+              child: Form(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    // Larger Logo
+                    Image.asset(
+                      'asset/logo.png',
+                      height: 150,
+                      width: 150,
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: TextFormField(
-                      obscureText: true, // For password fields
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16.0,
-                      ),
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        labelStyle: TextStyle(color: Colors.white),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.red),
-                        ),
-                        icon: Icon(Icons.lock, color: Colors.white),
-                        contentPadding: EdgeInsets.symmetric(horizontal: 10.0),
-                      ),
-                    ),
-                  ),
+                    const SizedBox(height: 16.0),
 
-                  // Login button
-                  const SizedBox(height: 16.0),
-                  ElevatedButton(
-                    onPressed: () {
-                      // Navigate to the ScannerWebPage
-                      Navigator.of(context)
-                          .pushReplacementNamed('/scanner-web');
-                    },
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.red, // Set Marvel-themed button color
-                      padding: EdgeInsets.symmetric(
-                          horizontal: 20.0, vertical: 12.0),
-                    ),
-                    child: Text(
-                      'Go to Scanner',
-                      style: GoogleFonts.robotoSlab(
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.w500,
+                    // Login text
+                    Text(
+                      'LOGIN',
+                      style: GoogleFonts.roboto(
+                        fontSize: 36.0,
+                        fontWeight: FontWeight.bold,
                         color: Colors.white,
                       ),
                     ),
-                  ),
+                    const SizedBox(height: 16.0),
 
-                  // Already have an account? Sign up option
-                  const SizedBox(height: 19.0),
-                  Text("Don't have an account?",
-                      style: GoogleFonts.robotoSlab(
+                    // Email text field
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: TextFormField(
+                        style: GoogleFonts.roboto(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                        ),
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          labelStyle: TextStyle(color: Colors.white),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.red),
+                          ),
+                          icon: Icon(Icons.email, color: Colors.white),
+                          contentPadding:
+                              EdgeInsets.symmetric(horizontal: 10.0),
+                        ),
+                      ),
+                    ),
+
+                    // Password text field
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: TextFormField(
+                        obscureText: true,
+                        style: GoogleFonts.roboto(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                        ),
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          labelStyle: TextStyle(color: Colors.white),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.red),
+                          ),
+                          icon: Icon(Icons.lock, color: Colors.white),
+                          contentPadding:
+                              EdgeInsets.symmetric(horizontal: 10.0),
+                        ),
+                      ),
+                    ),
+
+                    // Login button
+                    const SizedBox(height: 16.0),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Navigate to the ScannerWebPage
+                        Navigator.of(context)
+                            .pushReplacementNamed('/scanner-web');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.red,
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 16.0, vertical: 10.0),
+                      ),
+                      child: Text(
+                        'Go to Scanner',
+                        style: GoogleFonts.roboto(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+
+                    // Already have an account? Sign up option
+                    const SizedBox(height: 19.0),
+                    Text(
+                      "Don't have an account?",
+                      style: GoogleFonts.roboto(
                         color: Colors.white,
                         fontSize: 16.0,
                         fontWeight: FontWeight.w500,
-                      )),
-                  const SizedBox(height: 14.0),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.of(context).pushNamed('/sign-up');
-                    },
-                    style: TextButton.styleFrom(
-                      primary: Colors.white, // Set text color to white
-                    ),
-                    child: Text(
-                      'Sign Up',
-                      style: GoogleFonts.robotoSlab(
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.white,
                       ),
                     ),
-                  ),
-                ],
+                    const SizedBox(height: 14.0),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pushNamed('/sign-up');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.red,
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 16.0, vertical: 10.0),
+                      ),
+                      child: Text(
+                        'Sign Up',
+                        style: GoogleFonts.roboto(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+
+                    // Footer for credits
+                    const SizedBox(height: 10.0),
+                    Text(
+                      '© 2023 Ultron AI. All rights reserved. | Created by DEAK',
+                      style: GoogleFonts.roboto(
+                        color: Colors.white,
+                        fontSize: 12.0,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
